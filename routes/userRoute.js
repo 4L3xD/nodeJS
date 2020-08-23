c1c9421a04aaaa9771a3a45b95d46aeb982fa5ff
4L3_xD <alessandra.olea@gmail.com>
2020-08-23 18:41:40 -0300

    update code

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,25 +1,28 @@
 const fs = require('fs')
 const { join } = require('path')
 
-const filePath = join(__dirname, 'users.json')
+const filePath = join(__dirname, 'users.json');
 
 const getUsers = () => {
     const data = fs.existsSync(filePath)
         ? fs.readFileSync(filePath)
         : []
+
     try {
-        return JSON.parse(data)
-    } catch(error) {
-        return []
+        return JSON.parse(data);
+    } catch (e) {
+        return [];      
     }
-}
-const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users, null, '\t'))
-const userRoute = (app) => {
-    app.route("/users/:id?")
+};
+
+const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users, null, '\t'));
+
+const userRouter = (app) => {
+
+    app.route('/users/:id?')
         .get((req, res) => {
             const users = getUsers()
-    
-            res.send({ users })            
+            res.send({ users })
         })
         .post((req, res) => {
             const users = getUsers()
@@ -27,8 +30,29 @@ const userRoute = (app) => {
             users.push(req.body)
             saveUser(users)
 
-            res.status(201).send('Usuário cadastrado!')
+            res.status(201).send('OK')
+        })
+        .put((req, res) => {
+            const users = getUsers()
+            saveUser(users.map(user => {
+                if (user.id === req.params.id) {
+                    return {
+                        ...user,
+                        ...req.body
+                    }
+                }
+
+                return user
+            }))
+
+            res.status(200).send('OK')
+        })
+        .delete((req, res) => {
+            const users = getUsers()
+            saveUser(users.filter(user => user.id !== req.params.id))
+
+            res.status(200).send('OK')
         })
 }
 
-module.exports = userRoute
+module.exports = userRouter
